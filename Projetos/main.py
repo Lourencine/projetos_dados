48321eba904fac5698d515d44aeb94fe7560c626
Lourencine <dlourencine@gmail.com>
2024-11-28 20:59:11 -0300

    meu primeiro commit

---
+++ Projetos/main.py
@@ -0,0 +1,20 @@
+print("Jornada de dados")
+
+# Variáveis
+
+idade = 34
+nome = "Luciano"
+
+# Print
+print(idade)
+
+# Soma
+
+nova_idade = idade + 1
+print(nova_idade)
+
+# listas
+lista = [1,2,3,4,5]
+
+lista_invertida = lista.reverse()
+print(lista_invertida)
